3a82cff5b1e5da98d771ee19ba852abacbf5c88a
Jared Pines <31220098+Jaredpines@users.noreply.github.com>
2026-09-04 12:17:37 -0700

    Add solution - CodeHub

---
+++ 5-kyu/first-non-repeating-character/first-non-repeating-character.py
@@ -0,0 +1,13 @@
+from collections import Counter
+def first_non_repeating_letter(s):
+    first = ""
+    countText = Counter(list(s.lower()))
+    for key, value in countText.items():
+        if value == 1:
+            if key in s:
+                first = key
+            else:
+                first = key.upper()
+            break
+    print(countText)
+    return first
